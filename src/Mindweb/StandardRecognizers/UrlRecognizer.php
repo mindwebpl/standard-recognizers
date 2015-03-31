838b82a9ada4241b7ae212d4897208eb21f4fce2
Marcin Czołnowski <marcin@czolnowski.net>
2015-04-01 00:50:04 +0200

    Cast url to string, instead of object.

--- src/Mindweb/StandardRecognizers/UrlRecognizer.php
+++ src/Mindweb/StandardRecognizers/UrlRecognizer.php
@@ -29,7 +29,7 @@ class UrlRecognizer extends Recognizer
             );
         }
 
-        $attributionEvent->attribute('url', $url);
+        $attributionEvent->attribute('url', (string) $url);
     }
 
     /**
