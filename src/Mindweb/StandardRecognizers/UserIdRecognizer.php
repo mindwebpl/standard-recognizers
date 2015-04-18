3107720dd5f4616305b1727fa344a20b27993f21
Marcin Czołnowski <marcin@czolnowski.net>
2015-04-18 21:05:56 +0200

    Remove Pimple from UserId recognizer.

--- src/Mindweb/StandardRecognizers/UserIdRecognizer.php
+++ src/Mindweb/StandardRecognizers/UserIdRecognizer.php
@@ -1,10 +1,8 @@
 <?php
 namespace Mindweb\StandardRecognizers;
 
-use Mindweb\Config\Exception\InvalidConfigEntryException;
 use Mindweb\Recognizer\Event;
 use Mindweb\Recognizer\Recognizer;
-use Pimple;
 
 class UserIdRecognizer extends Recognizer
 {
@@ -12,18 +10,6 @@ class UserIdRecognizer extends Recognizer
      * @var array
      */
     private $userIds = array('_id', 'id', 'user_id');
-
-    /**
-     * @param Pimple $application
-     */
-    public function __construct(Pimple $application)
-    {
-        if (isset($application['configuration'])) {
-            try {
-                $this->userIds = $application['configuration']->get('tracker.userIds');
-            } catch (InvalidConfigEntryException $configException) {}
-        }
-    }
 
     /**
      * @param Event\AttributionEvent $attributionEvent
